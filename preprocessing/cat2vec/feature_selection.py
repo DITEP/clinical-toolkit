fe00383173f3fbf6b7661ab5353397ef810d3b35
vcharvet <valentin.charvet@telecom-paristech.fr>
2018-06-04 18:15:28 +0200

    [preprocessing.cat2vec] added feature_selection.py to select top variables

---
+++ preprocessing/cat2vec/feature_selection.py
@@ -0,0 +1,107 @@
+"""
+selects parameters with L1 logistic regression
+"""
+import pandas as pd
+
+from sklearn.base import BaseEstimator
+
+
+class LASSO_selector(BaseEstimator):
+    """
+    This class is made to be used after cat2vec.lasso_gridsearch since it
+    selects the features from a dataframe that have the most weighted
+    coefficients (according to a L1-penalized linear model)
+
+    It inherits from sklearn.base.BaseEstimator to allow gridsearching the
+    best `n_features` using a pipeline and a basline classifier
+
+    Parameters
+    ----------
+    n_features : int
+        number of top features to keep
+
+    lasso_coefs : pd.DataFrame
+        each row is the name of a category and its coef weight in LASSO
+        model
+
+    feature_col : str
+        name of the feature col (ie name of the categorical variable)
+
+    coef_col : str
+        name of the column of the LASSO coefficients in lasso_coefs dataframe
+
+    Examples      -
+    --------
+    >>> dico = {'coef': [0, 4.5, 1.2, 0.3], \
+                'colnames': ['feat1', 'feat2', 'feat3', 'feat4']}
+    >>> df = pd.DataFrame(dico)
+    keeps only feat2 and feat3
+    >>> selector = LASSO_selector(2).fit(df['colnames'], df['coef'])
+    >>> X = [[0, 0, 1, 0], [1, 1, 0, 0], [0, 1, 0, 0]]
+    >>> selector.transform(X)
+    [[0, 1], [1, 0], [1, 0]]
+    """
+    def __init__(self, lasso_coefs, feature_col, coef_col,
+                 n_features=64):
+        self.n_features = n_features
+        self.feature_col = feature_col
+        self.lasso_coefs = lasso_coefs
+        self.coef_col = coef_col
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        """
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            contains only features
+
+        Returns
+        -------
+        ndarray
+            contains the best n_features
+        """
+        self.lasso_coefs['abs_coef'] = abs(self.lasso_coefs[self.coef_col])
+        self.lasso_coefs.sort_values(['abs_coef'], ascending=False,
+                                     inplace=True)
+
+        # keeping top features according to lasso
+        coefs_to_keep = self.lasso_coefs.iloc[:self.n_features, :]
+        coefs_to_keep = coefs_to_keep[self.feature_col]
+
+        return X[coefs_to_keep.values].values
+
+    # # following methods exist for scikit-learn pipeline and gridsearch APIs
+    # deprecated since BaseEstimator inheritance
+    # def get_params(self, deep=True):
+    #     return {'lasso_coefs': self.lasso_coefs,
+    #             'n_features': self.n_features}
+    #
+    # def set_params(self, **parameters):
+    #         for parameter, value in parameters.items():
+    #             setattr(self, parameter, value)
+    #         return self
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ preprocessing/cat2vec/tests/feature_selection_test.py
@@ -0,0 +1,59 @@
+"""
+
+
+"""
+import pandas as pd
+import numpy as np
+
+from preprocessing.cat2vec.feature_selection import LASSO_selector
+from numpy.testing import assert_array_equal
+
+
+values = {'feature1': [0, 0, 1, 1, 0],
+          'feature2': [0, 1, 1, 0, 1],
+          'feature3': [1, 0, 0, 0, 0],
+          'feature4': [1, 0, 0, 0, 1]}
+
+coefficients = {'coef': [0, 4.5, -1.2, 0.5],
+                'feature_name': ['feature1', 'feature2', 'feature3',
+                                 'feature4']}
+
+
+df = pd.DataFrame(values)
+#         feature1  feature2  feature3  feature4
+# 0         0         0         1         1
+# 1         0         1         0         0
+# 2         1         1         0         0
+# 3         1         0         0         0
+# 4         0         1         0         1
+
+df_coef = pd.DataFrame(coefficients)
+#     coef    feature_name
+# 0   0.0     feature1
+# 1   4.5     feature2
+# 2  -1.2     feature3
+# 3   0.5     feature4
+
+
+class TestTransformation(object):
+    def SetUp(self):
+        return self
+
+    def test_fit_transform(self):
+        selector = LASSO_selector(n_features=2,
+                                  lasso_coefs=df_coef,
+                                  feature_col='feature_name',
+                                  coef_col='coef')
+        # selector.fit(df_coef.feature_name, df_coef.coef)
+
+        x_res = selector.transform(df)
+
+        x_expected = np.array([[0, 1], [1, 0], [1, 0], [0, 0], [1, 0]])
+
+        assert_array_equal(x_expected, x_res)
+
+
+
+
+
+
